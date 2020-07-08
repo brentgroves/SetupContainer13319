2883dff322cb46bc48f8e072f6ef29667c212b54
Brent Groves <brent.groves@gmail.com>
2020-07-08 10:07:50 -0400

    Added/Changed Code

---
+++ app.js
@@ -0,0 +1,76 @@
+const mariadb = require("mariadb");
+const mqtt = require('mqtt');
+const common = require('@bgroves/common');
+const utils = require('./utils');
+
+const {
+  MQTT_SERVER,
+  MYSQL_HOSTNAME,
+  MYSQL_USERNAME,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE
+} = process.env;
+
+/*
+const MQTT_SERVER='localhost';
+const MYSQL_HOSTNAME= "localhost";
+const MYSQL_USERNAME= "brent";
+const MYSQL_PASSWORD= "JesusLives1!";
+const MYSQL_DATABASE= "mach2";
+*/
+const connectionString = {
+    connectionLimit: 5,
+    multipleStatements: true,
+    host: MYSQL_HOSTNAME,
+    user: MYSQL_USERNAME,
+    password: MYSQL_PASSWORD,
+    database: MYSQL_DATABASE
+  }
+  
+common.log(`user: ${MYSQL_USERNAME},password: ${MYSQL_PASSWORD}, database: ${MYSQL_DATABASE}, MYSQL_HOSTNAME: ${MYSQL_HOSTNAME}`);
+  
+const pool = mariadb.createPool( connectionString);
+  
+
+async function InsertSetupContainer(params) {
+    var p = utils.conversions(params);
+    let conn;
+    try {
+      conn = await pool.getConnection();      
+      const result = await conn.query('call InsertSetupContainer(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+      [p.TransDate,p.PCN,p.ProdServer,p.Workcenter,p.CNC,p.Part_No,p.Name,p.Multiple,
+       p.Container_Note,p.Cavity_Status_Key,p.Container_Status,
+       p.Defect_Type,p.Serial_No,p.Setup_Container_Key,p.Count, 
+       p.Part_Count,p.Part_Key,p.Part_Operation_Key,p.Standard_Container_Type,p.Container_Type_Key,
+       p.Parent_Part,p.Parent,p.Cavity_No,p.Master_Unit_Key,p.Workcenter_Printer_Key,
+       p.Master_Unit_No,p.Physical_Printer_Name,p.Container_Count,p.Container_Quantity,
+       p.Default_Printer,p.Default_Printer_Key,p.Class_Key,p.Quantity,p.Companion,
+       p.Container_Type,p.Container_Type_Description,p.Sort_Order]);
+      let msgString = JSON.stringify(result[0]);
+      const obj = JSON.parse(msgString.toString()); // payload is a buffer
+      common.log(obj);
+    } catch (err) {
+      // handle the error
+      console.log(`Error =>${err}`);
+    } finally {
+      if (conn) conn.release(); //release to pool
+    }
+  }
+  
+function main() {
+  let mqttClient = mqtt.connect(MQTT_SERVER);
+
+  mqttClient.on('connect', function() {
+    mqttClient.subscribe('Plex13319', function(err) {
+      if (!err) {
+        console.log('subscribed to: Plex13319');
+      }
+    });
+  });
+  // message is a buffer
+  mqttClient.on('message', function(topic, message) {
+    const params = JSON.parse(message.toString()); // payload is a buffer
+    InsertSetupContainer(params);
+  });
+}
+main();
---
+++ utils.js
@@ -0,0 +1,48 @@
+module.exports.conversions = function(params) {
+  var retParams = params;
+
+  var propTypes = {
+    Control_Panel_Setup_Containers_Get_Key: 'int',
+    TransDate: 'datetime',
+    ProdServer: 'bool',
+    Part_No: 'varchar',
+    Name: 'varchar',
+    Multiple: 'bool',
+    Container_Note: 'varchar',
+    Cavity_Status_Key: 'int',
+    Container_Status: 'varchar',
+    Defect_Type: 'varchar',
+    Serial_No: 'varchar',
+    Setup_Container_Key: 'int',
+    Count: 'mediumint',
+    Part_Count: 'mediumint',
+    Part_Key: 'int',
+    Part_Operation_Key: 'int',
+    Standard_Container_Type: 'varchar',
+    Container_Type_Key: 'int',
+    Parent_Part: 'varchar',
+    Parent: 'varchar',
+    Cavity_No: 'varchar',
+    Master_Unit_Key: 'int',
+    Workcenter_Printer_Key: 'int',
+    Master_Unit_No: 'varchar',
+    Physical_Printer_Name: 'varchar',
+    Container_Count: 'mediumint',
+    Container_Quantity: 'mediumint',
+    Default_Printer: 'varchar',
+    Default_Printer_Key: 'int',
+    Class_Key: 'int',
+    Quantity: 'int',
+    Companion: 'bool',
+    Container_Type: 'varchar',
+    Container_Type_Description: 'varchar',
+    Sort_Order: 'mediumint',
+  };
+
+  for (var prop in retParams) {
+    if ('mediumint' == propTypes[prop] && '' == retParams[prop]) retParams[prop] = null;
+    if ('int' == propTypes[prop] && '' == retParams[prop]) retParams[prop] = null;
+    // console.log(prop + ' ' + retParams[prop]);
+  }
+  return retParams;
+};
